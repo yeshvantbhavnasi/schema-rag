41b1f4f55b9f5e75e6422e764aef3d7e44390b4b
Yeshvant Bhavnasi <devyeshvant@gmail.com>
2024-11-11 12:42:55 -0800

    fix: sample data for schema and data-dictionary

---
+++ src/model/canonical_type.py
@@ -0,0 +1,8 @@
+
+class CanonicalType:
+    name: str
+    type: str
+    doc: str
+    data_dictionary: str
+    default_classification: str
+    enhanced_classification: str
---
+++ src/model/data_dictionary.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class DataDictionary:
+    name: str
+    type: str
+    doc: str
+    default_classification: str
+    enhanced_classification: str
+    gcp_info_type: List[str]
