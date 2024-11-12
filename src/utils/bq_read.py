31b6f6b47c2e81456d9b3b0f8dbfc7f3a5c5933a
Yeshvant Bhavnasi <devyeshvant@gmail.com>
2024-11-11 21:44:47 -0800

    fix: implement bq read functionality

--- src/utils/bq_read.py
+++ src/utils/bq_read.py
@@ -1,9 +1,25 @@
+from google.cloud.bigquery import Client
+
+
 class BQRead:
     def __init__(self, config):
         self.config = config
+        self.bq_client = Client.from_service_account_json(
+            config['bigquery']['key_path']
+        )
 
     def read_table(self, table_id):
-        pass
+        try:
+            dataset = self.bq_client.dataset(self.config.bigquery.dataset_id)
+            # read table
+            table = dataset.table(table_id)
+            return table.to_dataframe()
+        except Exception as e:
+            # Log the error or handle it as needed
+            print(f"An error occurred while reading the table: {e}")
+            # Optionally, re-raise the exception or return a default value
+            raise
 
     def export_data_to_gcs(self, table_id):
+        # todo: implement gcs export
         pass
---
+++ tests/utils/test_bq_read.py
@@ -0,0 +1,32 @@
+from src.utils.bq_read import BQRead
+from unittest.mock import patch
+import pandas as pd
+import pytest
+
+
+def test_bq_read():
+    mock_data = pd.DataFrame({
+        'column1': [1, 2, 3],
+        'column2': ['a', 'b', 'c']
+    })
+    config = {
+        "bigquery": {
+            "key_path": "./config/keys/service_account.json",
+        }
+    }
+    with patch('src.utils.bq_read.BQRead.read_table', return_value=mock_data):
+        bq_read = BQRead(config)  # Pass the config here
+        df = bq_read.read_table("schema_rag.schema_rag")
+        print(df.head())
+        assert df.equals(mock_data)
+
+
+def test_bq_read_error():
+    config = {
+        "bigquery": {
+            "key_path": "./config/keys/service_account.json",
+        }
+    }
+    bq_read = BQRead(config)
+    with pytest.raises(Exception):
+        bq_read.read_table("schema_rag.schema_rag")
